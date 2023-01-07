1e024ad75e74182e5b48ad5c8ccf23478bf5a50b
Nick Exadaktylos <nick6finger@gmail.com>
2023-01-07 15:39:01 +0200

    fix ambiguity with windows Rectangle

--- test/mbr_test.cpp
+++ test/mbr_test.cpp
@@ -14,13 +14,13 @@ TEST_CASE("MBR tests")
       auto [top_left, bottom_right] = GENERATE(
           p {Point {1, 2}, Point {2, 3}},
           p {Point {3, 4}, Point {4.32, 64.25}});
-      Rectangle rect {top_left, bottom_right};
+      gis::Rectangle rect {top_left, bottom_right};
 
       CHECK(MBR(rect) == rect);
    }
    SECTION("The MBR of a line is a rectangle to which the line is a diagonal")
    {
-      using p = std::pair<LineSegment, Rectangle>;
+      using p = std::pair<LineSegment, gis::Rectangle>;
       auto [line, rect] = GENERATE(
           p {{{0, 2}, {2, 3}}, {{0, 2}, {2, 3}}},
           p {{{3, 4}, {4, 0.234}}, {{3, 0.234}, {4, 4}}},
