d8a2465dd1825262a96078b72cf7719153b42bf8
Nick Exadaktylos <nick6finger@gmail.com>
2023-01-09 10:56:15 +0200

    overload 2D vector operators and  implement distance for point

--- include/gis-utils/point.h
+++ include/gis-utils/point.h
@@ -1,5 +1,8 @@
+#include <cassert>
+#include <cmath>
 #include <compare>
 #include <tuple>
+#include <type_traits>
 
 #ifndef POINT_H
 #define POINT_H
@@ -7,6 +10,9 @@
 namespace gis
 {
 using coord_t = double;
+
+template <typename T>
+concept Arithmetic = std::integral<T> or std::floating_point<T>;
 
 struct Point
 {
@@ -23,6 +29,46 @@ inline bool lex_comp_less(const Point& lhs, const Point& rhs)
 {
    return std::tie(lhs.x, lhs.y) < std::tie(rhs.x, rhs.y);
 };
+
+inline Point operator+(const Point& lhs, const Point& rhs)
+{
+   return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+inline Point operator-(const Point& lhs, const Point& rhs)
+{
+   return {lhs.x - rhs.x, lhs.y - rhs.y};
+}
+
+inline Point operator*(const Point& p, Arithmetic auto scalar)
+{
+   return {p.x * scalar, p.y * scalar};
+}
+inline Point operator*(Arithmetic auto scalar, const Point& p)
+{
+   return p * scalar;
+}
+
+inline Point operator/(const Point& p, Arithmetic auto scalar)
+{
+   assert(scalar != 0);
+   return {p.x / scalar, p.y / scalar};
+}
+
+inline coord_t dot_product(const Point& lhs, const Point& rhs)
+{
+   return lhs.x * rhs.x + lhs.y * rhs.y;
+}
+
+inline coord_t distance_squared(const Point& p1, const Point& p2)
+{
+   return dot_product(p1 - p2, p1 - p2);
+}
+
+inline coord_t distance(const Point& p1, const Point& p2)
+{
+   return std::sqrt(distance_squared(p1, p2));
+}
 
 enum class Orientation
 {
